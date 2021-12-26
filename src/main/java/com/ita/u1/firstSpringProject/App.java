5de3ed4cdde7cc3155e1de27ed72d490f000b11c
Anton <antondanik32@mail.ru>
2021-12-26 17:09:48 +0300

    added xml config

--- src/main/java/com/ita/u1/firstSpringProject/App.java
+++ src/main/java/com/ita/u1/firstSpringProject/App.java
@@ -1,7 +1,15 @@
 package com.ita.u1.firstSpringProject;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml"});
 
+        MovieRepository movieRepository = context.getBean(MovieRepository.class);
+
+        MovieReport movieReport = new MovieReport(movieRepository);
+        movieReport.generate();
     }
 }
