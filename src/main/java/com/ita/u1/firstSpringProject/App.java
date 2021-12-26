084ca907248a90cb4f3380529c60b69cc95c978d
Anton <antondanik32@mail.ru>
2021-12-26 16:56:51 +0300

    fixed class App

--- src/main/java/com/ita/u1/firstSpringProject/App.java
+++ src/main/java/com/ita/u1/firstSpringProject/App.java
@@ -1,7 +1,15 @@
 package com.ita.u1.firstSpringProject;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigApp.class);
 
+        MovieRepository movieRepository = context.getBean(MovieRepository.class);
+
+        MovieReport movieReport = new MovieReport(movieRepository);
+        movieReport.generate();
     }
 }
